3484f7196c3a4522a568a6f1df9ec4fbce82864e
Afrian Pasa <octavarium07@gmail.com>
2018-09-05 14:23:57 +0700

    first commit

---
+++ src/main/resources/data/mysql/migrations/V0_1_1__migration.sql
@@ -0,0 +1,15 @@
+/*
+ * Engine: MySQL
+ * Version: 0.1.0
+ * Description: 
+ *   Database changes for version 0.1.0.
+ */
+
+/*
+ * Structure
+ */
+
+/*
+ * Data
+ */
+INSERT INTO Greeting (referenceId, text, version, createdBy, createdAt, updatedBy, updatedAt) VALUES ('e1707cf7-2aa4-4745-b04f-c9e03dc0a660', 'Halooooo', 0, 'user', NOW(), NULL, NULL);
